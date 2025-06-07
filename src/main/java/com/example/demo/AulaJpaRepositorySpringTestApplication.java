4f1324704059de5d472f84ef0c8f3f2fec108ef1
Samyr Tertuliano <samyrtertuliano4@gmail.com>
2025-06-07 16:55:36 -0300

    Jpa interface structure

---
+++ src/main/java/com/example/demo/AulaJpaRepositorySpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaJpaRepositorySpringTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaJpaRepositorySpringTestApplication.class, args);
+	}
+
+}
